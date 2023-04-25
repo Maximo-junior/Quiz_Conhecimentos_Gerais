89d75beb2d4735ffc99a107c2422096e92b5bbac
Máximo Júnior <juniormaxmt@gmail.com>
2023-04-25 08:18:14 -0300

    subindo projeto

---
+++ src/data/questions.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    question: "Qual é o estado brasileiro com o maior litoral ?",
+    options: ["Bahia", "Rio de Janeiro", "Espirito Santo", "São Paulo"],
+    answer: "Bahia",
+  },
+  {
+    question: "Qual é o rio mais extenso do Brasil ?",
+    options: ["Negro", "Amazonas", "Doce", "Jacu"],
+    answer: "Amazonas",
+  },
+  {
+    question:
+      "Qual é o estado brasileiro que faz fronteira com o maior número de países sul-americanos?",
+    options: ["Mato Grosso do Sul", "Rondonia", "Bahia", "Rio de janeiro"],
+    answer: "Mato Grosso do Sul",
+  },
+  {
+    question:
+      "Qual é o nome da região do Brasil que é conhecida por suas paisagens de chapadas e cavernas?",
+    options: ["Deserta", "Chapadas", "Mata", "Cerrado"],
+    answer: "Cerrado",
+  },
+  {
+    question:
+      "Qual é o nome da cidade brasileira que é conhecida como a cidade maravilhosa?",
+    options: ["Rio de Janeiro", "Copa cabana", "Bahia", "Espirito Santo"],
+    answer: "Rio de Janeiro",
+  },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,106 @@
+const data = [
+  {
+    category: "Tecnologia",
+    questions: [
+      {
+        question:
+          "Qual é a linguagem de programação mais usada no desenvolvimento web?",
+        options: ["PHP", "C#", "Javascript", "Delphi"],
+        answer: "Javascript",
+        tip: "É amiga do React",
+      },
+      {
+        question:
+          "Qual é o nome do sistema operacional de código aberto baseado em Linux e usado principalmente em servidores?",
+        options: ["Windows", "Ubuntu", "IOS", "Linux"],
+        answer: "Ubuntu",
+        tip: "Tem a letra U",
+      },
+      {
+        question:
+          "Qual é o nome do sistema de inteligência artificial desenvolvido pela IBM?",
+        options: ["Watson", "Flutter", "Delphi", "Windows"],
+        answer: "Watson",
+      },
+      {
+        question:
+          "Qual é o nome do comando usado para criar uma nova pasta em um sistema operacional de linha de comando?",
+        options: ["placeholder", "mkdir", "required", "maxlength"],
+        answer: "mkdir",
+      },
+      {
+        question:
+          "Qual é o framework de JavaScript usado para criar interfaces de usuário interativas e dinâmicas?",
+        options: ["React", "PHP", "Mskl", "Bulma"],
+        answer: "React",
+      },
+    ],
+  },
+  {
+    category: "História",
+    questions: [
+      {
+        question: "Em que ano a Proclamação da República aconteceu?",
+        options: ["1889", "1887", "1882", "1788"],
+        answer: "1889",
+        tip: "Número impar",
+      },
+      {
+        question:
+          "Qual o nome do presidente que liderou o país durante a Segunda Guerra Mundial?",
+        options: [
+          "Getúlio silva",
+          "Pedro alvares Cabral",
+          " Getúlio Vargas",
+          "Bruno Henrique",
+        ],
+        answer: " Getúlio Vargas",
+      },
+      {
+        question: "Qual foi o líder da Inconfidência Mineira?",
+        options: [
+          "Amaral peixoto",
+          "Cezar Ribeiro",
+          "Lucio Mauro",
+          "Tiradentes",
+        ],
+        answer: "Tiradentes",
+      },
+    ],
+  },
+  {
+    category: "Geografia ",
+    questions: [
+      {
+        question: "Qual é o estado brasileiro com o maior litoral ?",
+        options: ["Bahia", "Rio de Janeiro", "Espirito Santo", "São Paulo"],
+        answer: "Bahia",
+      },
+      {
+        question: "Qual é o rio mais extenso do Brasil ?",
+        options: ["Negro", "Amazonas", "Doce", "Jacu"],
+        answer: "Amazonas",
+      },
+      {
+        question:
+          "Qual é o estado brasileiro que faz fronteira com o maior número de países sul-americanos?",
+        options: ["Mato Grosso do Sul", "Rondonia", "Bahia", "Rio de janeiro"],
+        answer: "Mato Grosso do Sul",
+      },
+      {
+        question:
+          "Qual é o nome da região do Brasil que é conhecida por suas paisagens de chapadas e cavernas?",
+        options: ["Deserta", "Chapadas", "Mata", "Cerrado"],
+        answer: "Cerrado",
+      },
+      {
+        question:
+          "Qual é o nome da cidade brasileira que é conhecida como a cidade maravilhosa?",
+        options: ["Rio de Janeiro", "Copa cabana", "Bahia", "Espirito Santo"],
+        answer: "Rio de Janeiro",
+      },
+    ],
+  },
+];
+
+export default data;
